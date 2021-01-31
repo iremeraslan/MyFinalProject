1d48c098620b8b9112865d4bbe46039c8bb059c9
irem eraslan <harmocoder@gmail.com>
2021-01-31 10:19:55 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/context.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    internal class context
+    {
+        internal static object Entry(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
